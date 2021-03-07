6bc74066840a4511ab51a5b1134c21950715d949
Volodymyr_Moisei <volodymyr.moisei@gmail.com>
2021-03-07 23:29:52 +0200

    CustomerGetAllComponent.jsx => CreateButton + CustomerList + GetById

--- src/main/webapp/reactjs/src/customer/components/CustomerGetAllComponent.jsx
+++ src/main/webapp/reactjs/src/customer/components/CustomerGetAllComponent.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import CustomerService from "../service/CustomerService";
+import CustomerList from "./GetAllComponents/CustomerList";
+import CreateButton from "./GetAllComponents/CreateButton"
+import GetById from "./GetAllComponents/GetById";
 
 class CustomerGetAllComponent extends Component {
     constructor(props) {
@@ -19,48 +22,9 @@ class CustomerGetAllComponent extends Component {
     render() {
         return (
             <div>
-                <table className = "table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>ПІП</th>
-                            <th>Телефон</th>
-                            <th>Місце проживання</th>
-                            <th>Адреса</th>
-                            <th>Номер паспорту</th>
-                            <th>description</th>
-                            <th>created_at</th>
-                            <th>modified_at</th>
-                            <th>delete</th>
-                            <th>edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.customers.map(
-                            customer =>
-                                <tr key={customer.id}>
-                                    <td>{customer.id}</td>
-                                    <td>{customer.name}</td>
-                                    <td>{customer.telephone}</td>
-                                    <td>{customer.address}</td>
-                                    <td>{customer.placeOfResidence}</td>
-                                    <td>{customer.passport}</td>
-                                    <td>{customer.description}</td>
-                                    <td>{customer.created_at}</td>
-                                    <td>{customer.modified_at}</td>
-                                    <td>
-                                        <form action="" method="post">
-                                            <button className="btn btn-warning mt-1" type="submit">Delete</button>
-                                        </form>
-                                    </td>
-                                    <td><a href=""
-                                           className="btn btn-warning mt-1">Edit</a></td>
-                                </tr>
-                        )
-                    }
-                    </tbody>
-                </table>
+               <CustomerList customers = {this.state.customers}/>
+               <CreateButton />
+               <GetById />
             </div>
         )
     }
