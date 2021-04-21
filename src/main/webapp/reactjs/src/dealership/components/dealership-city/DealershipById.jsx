4dd44def9eb9da44cfde5967cac33191b73b4e12
Volodymyr_Moisei <volodymyr.moisei@gmail.com>
2021-04-21 11:41:16 +0300

    added loading-spinner

--- src/main/webapp/reactjs/src/dealership/components/dealership-city/DealershipById.jsx
+++ src/main/webapp/reactjs/src/dealership/components/dealership-city/DealershipById.jsx
@@ -1,15 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import DealershipService from "../../service/DealershipService";
+import {Spinner} from "react-bootstrap";
 
 const DealershipById = (props) => {
-    const [id,setId] = useState(props.dealershipId);
+    const [id, setId] = useState(props.dealershipId);
     const [dealership, setDealership] = useState({});
 
     useEffect(() => {
         setId(props.dealershipId);
         DealershipService.getById(id).then(result => setDealership(result.data))
     })
-
+    if (dealership.id !== props.dealershipId) {
+        return (
+            <Spinner animation="border"/>
+        )
+    }
     return (
         <div>
             {dealership.city}
